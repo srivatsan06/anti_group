f04c1b32fcf6cb61ec51ddfba7ea0b520358da04
ssrivatsan06 <ssrivatsan06@gmail.com>
2025-12-01 02:51:49 +0000

    Remote deploy

--- controllers/admin_controller.py
+++ controllers/admin_controller.py
@@ -44,7 +44,7 @@ class AdminController:
         # Hash password
         hash_pass = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         
-        return self.user_model.create(user_id, user_name, role, email, hash_pass)
+        return self.user_model.create(user_id, user_name, role, hash_pass, email)
 
     def get_all_users(self):
         """View all users."""
